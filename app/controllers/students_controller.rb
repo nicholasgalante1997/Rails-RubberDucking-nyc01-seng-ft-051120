8172c146cb5802a36b267c1b452f9c4fe45aa934
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-06-15 19:26:40 -0400

    final

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,34 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show 
+    @student = Student.find(params[:id])
+  end 
+
+  def new 
+    @student = Student.new 
+  end 
+
+  def create 
+    @student = Student.create(student_params)
+    if @student.valid?
+      redirect_to student_path(@student)
+    else 
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end 
+  end 
+
+  def destroy 
+    @student = Student.find(params[:id])
+    @student.destroy 
+    redirect_to students_path 
+  end 
+  private 
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end 
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ApplicationRecord
+    has_many :ducks 
+    validates :name, length: { minimum: 1 }
+    validates :mod, numericality: { greater_than: 0 }
+    validates :mod, numericality: { less_than_or_equal_to: 5}
 end
